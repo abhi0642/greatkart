a3dc55c9ae0b41a80cdfc40ebc0296e8a8096cb2
abhi0642 <abhi8962547434@gmail.com>
2021-11-25 17:15:02 +0530

    first commit

---
+++ greatkart/category/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=40,unique=True)
+    slug=models.CharField(max_length=100,unique=True)
+    description=models.CharField(max_length=700,blank=True)
+    cat_image=models.ImageField(upload_to='photoes/category',blank=True)
+
+    class Meta:
+        verbose_name='Category'
+        verbose_name_plural='Catogories'
+
+    def __str__(self):
+        return self.category_name
